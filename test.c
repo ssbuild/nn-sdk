aaf1ad72759f21eba479e9be09cda455f6470951
ssbuild <86179625+ssbuild@users.noreply.github.com>
2021-09-13 22:16:21 +0800

    Update test.c

--- test.c
+++ test.c
@@ -5,22 +5,26 @@ int main(){
     if (0 != sdk_init_cc()) {
 		return -1;
 	}
-	const char* json_data = "{	\n\
-	\"model_dir\": \"/root/model.ckpt\",\n\
-	\"log_level\":8, \n\
-	\"model_type\":1, \n\
-	\"ConfigProto\": {\n\
-		\"log_device_placement\":0,\n\
-		\"allow_soft_placement\":1,\n\
-		\"gpu_options\":{\"allow_growth\": 1}\n\
-	},\n\
-	\"graph_inf_version\": 1,\n\
-	\"graph\": [\n\
-		{\n\
-			\"input\": [{\"node\":\"input_ids:0\", \"data_type\":\"float\", \"shape\":[1, 10]}],\n\
-			\"output\" : [{\"node\":\"pred_ids:0\", \"data_type\":\"float\", \"shape\":[1, 10]}]\n\
-		}\n\
-	]\n\
+    printf("配置参考 python.........\n");
+	const char* json_data = "{\n\
+    \"model_dir\": \"/root/model.ckpt\",\n\
+    \"log_level\":8, \n\
+     \"device_id\":0, \n\
+    \"tf\":{ \n\
+         \"ConfigProto\": {\n\
+            \"log_device_placement\":0,\n\
+            \"allow_soft_placement\":1,\n\
+            \"gpu_options\":{\"allow_growth\": 1}\n\
+        },\n\
+        \"engine_version\": 1,\n\
+        \"model_type\":1 ,\n\
+    },\n\
+    \"graph\": [\n\
+        {\n\
+            \"input\": [{\"node\":\"input_ids:0\", \"data_type\":\"float\", \"shape\":[1, 10]}],\n\
+            \"output\" : [{\"node\":\"pred_ids:0\", \"data_type\":\"float\", \"shape\":[1, 10]}]\n\
+        }\n\
+    ]\n\
 }";
 	printf("%s\n", json_data);
 	auto handle = sdk_new_cc(json_data);
@@ -36,13 +40,15 @@ int main(){
 		result[i] = (float*)malloc(M * N * element_output_size);
 		memset(result[i], 0, M * N * element_output_size);
 	}
-	input[0] = (int*)malloc(M * N * element_input_size);
-	input[1] = (int*)malloc(M * N * element_input_size);
-	for (int i = 0; i < N; ++i) {
-		input[0][i] = i;
-		input[1][i] = 1;
+	for(int i =0;i<INPUT_NUM;++i){
+		input[i] = (int*)malloc(M * N * element_input_size);
+		memset(input[i], 0, M * N * element_input_size);
+		for (int j = 0; j < N; ++j) {
+			input[i][j] = i;
+		}
 	}
-	int code = sdk_process_cc(handle, (void**)result, 0, INPUT_NUM, input[0]);
+
+	int code = sdk_process_cc(handle, (void**)result, 0, (void**)input);
 	if (code == 0) {
 		printf("result\n");
 		for (int i = 0; i < N; ++i) {
